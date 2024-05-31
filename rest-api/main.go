e1ddd0f6eff310fb9d09778483d93f54a890f518
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-05-31 11:05:13 +0530

    Update

---
+++ rest-api/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// User represents our data model
+type User struct {
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name     string             `json:"name"`
+	Age      int                `json:"age"`
+	Location string             `json:"location"`
+}
+
+var collection *mongo.Collection
+
+func main() {
+	app := fiber.New()
+
+	// Connect to MongoDB
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection = client.Database("user-db").Collection("users")
+
+	// Define routes
+	app.Get("/users", getUsers)
+	app.Get("/users/:id", getUser)
+	app.Post("/users", createUser)
+	app.Put("/users/:id", updateUser)
+	app.Delete("/users/:id", deleteUser)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+// GET
+func getUsers(c *fiber.Ctx) error {
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+	defer cursor.Close(context.Background())
+
+	var users []User
+	if err := cursor.All(context.Background(), &users); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(users)
+}
+
+func getUser(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	var user User
+	err = collection.FindOne(context.Background(), bson.M{"_id": id}).Decode(&user)
+	if err != nil {
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+	}
+
+	return c.JSON(user)
+}
+
+// POST
+func createUser(c *fiber.Ctx) error {
+	var user User
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	_, err := collection.InsertOne(context.Background(), user)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"message": "User created successfully"})
+}
+
+// PUT
+func updateUser(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	var user User
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	_, err = collection.UpdateOne(context.Background(), bson.M{"_id": id}, bson.M{"$set": user})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"message": "User updated successfully"})
+}
+
+// DELETE
+func deleteUser(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	_, err = collection.DeleteOne(context.Background(), bson.M{"_id": id})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"message": "User deleted successfully"})
+}
